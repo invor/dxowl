061d4e42a5fd27281a5d5d1bf4a3c8237f1fa9f1
becherml <michael.becher@visus.uni-stuttgart.de>
2023-10-26 17:20:57 +0200

    Add a fishy resize function

--- include/dxowl/RenderTarget.hpp
+++ include/dxowl/RenderTarget.hpp
@@ -21,6 +21,8 @@ namespace dxowl
             D3D11_RENDER_TARGET_VIEW_DESC const &rndr_tgt_view);
         ~RenderTarget(){};
 
+        void resize(ID3D11Device4* d3d11_device, UINT width, UINT height);
+
         inline Microsoft::WRL::ComPtr<ID3D11RenderTargetView> getRenderTargetView() const
         {
             return m_rndr_tgt_view;
@@ -45,6 +47,38 @@ namespace dxowl
             &m_rndr_tgt_view_desc,
             &m_rndr_tgt_view);
     }
+
+    inline void RenderTarget::resize(ID3D11Device4* d3d11_device, UINT width, UINT height)
+    {
+        m_rndr_tgt_view = nullptr;
+        m_shdr_rsrc_view = nullptr;
+        m_texture = nullptr;
+
+        m_rndr_tgt_view.Reset();
+        m_shdr_rsrc_view.Reset();
+        m_texture.Reset();
+
+        m_desc.Width = width;
+        m_desc.Height = height;
+
+        HRESULT hr = d3d11_device->CreateTexture2D(
+            &m_desc,
+            nullptr,
+            m_texture.GetAddressOf()
+        );
+
+        hr = d3d11_device->CreateShaderResourceView(
+            m_texture.Get(),
+            &m_shdr_rsrc_view_desc,
+            m_shdr_rsrc_view.GetAddressOf()
+        );
+
+        hr = d3d11_device->CreateRenderTargetView(
+            m_texture.Get(),
+            &m_rndr_tgt_view_desc,
+            &m_rndr_tgt_view);
+    }
+
 } // namespace dxowl
 
 #endif
