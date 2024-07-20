ae7840226a2fb29bcda1b733ad8fad62791227ce
invor <1233213+invor@users.noreply.github.com>
2024-07-20 03:03:10 +0200

    Update Buffer.hpp

--- include/dxowl/Buffer.hpp
+++ include/dxowl/Buffer.hpp
@@ -23,28 +23,47 @@ namespace dxowl
         Buffer(
             ID3D11Device4* d3d11_device,
             D3D11_BUFFER_DESC const& buffer_desc,
+            D3D11_SHADER_RESOURCE_VIEW_DESC const& shdr_rsrc_view,
             Container const& datastorage)
-            : m_buffer(nullptr), m_descriptor(buffer_desc)
+            : m_buffer(nullptr), m_descriptor(buffer_desc), m_shdr_rsrc_view_desc(shdr_rsrc_view)
         {
-            size_t byte_size = datastorage.size() * sizeof(decltype(datastorage)::value_type);
+            size_t byte_size = datastorage.size() * sizeof(Container::value_type);
+
+            HRESULT hr;
 
             if (byte_size > 0)
             {
                 D3D11_SUBRESOURCE_DATA init_data;
                 init_data.pSysMem = datastorage.data();
-                return d3d11_device->CreateBuffer(&m_descriptor, &init_data, &m_buffer);
+                hr = d3d11_device->CreateBuffer(&m_descriptor, &init_data, &m_buffer);
             }
             else
             {
-                return d3d11_device->CreateBuffer(&m_descriptor, nullptr, &m_buffer);
+                hr = d3d11_device->CreateBuffer(&m_descriptor, nullptr, &m_buffer);
             }
+
+            //TODO do something with hr
+
+            hr = d3d11_device->CreateShaderResourceView(
+                m_buffer.Get(),
+                &m_shdr_rsrc_view_desc,
+                m_shdr_rsrc_view.GetAddressOf());
+
+            //TODO do something with hr
         }
 
         ~Buffer() = default;
 
+        inline Microsoft::WRL::ComPtr<ID3D11ShaderResourceView> getShaderResourceView() {
+            return m_shdr_rsrc_view;
+        }
+
     private:
         Microsoft::WRL::ComPtr<ID3D11Buffer> m_buffer;
         D3D11_BUFFER_DESC m_descriptor;
+
+        Microsoft::WRL::ComPtr<ID3D11ShaderResourceView> m_shdr_rsrc_view;
+        D3D11_SHADER_RESOURCE_VIEW_DESC m_shdr_rsrc_view_desc;
     };
 } // namespace dxowl
 
